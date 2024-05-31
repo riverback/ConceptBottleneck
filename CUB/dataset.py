f1b6869591afcaf9658968d4abeba40104c1c679
riverback <hhz_pku@163.com>
2024-05-31 15:59:53 +0000

    change data path for bei qing road server

--- CUB/dataset.py
+++ CUB/dataset.py
@@ -50,12 +50,12 @@ class CUBDataset(Dataset):
         img_path = img_data['img_path']
         # Trim unnecessary paths
         try:
-            idx = img_path.split('/').index('CUB_200_2011')
-            if self.image_dir != 'images':
-                img_path = '/'.join([self.image_dir] + img_path.split('/')[idx+1:])
-                img_path = img_path.replace('images/', '')
-            else:
-                img_path = '/'.join(img_path.split('/')[idx:])
+            # idx = img_path.split('/').index('CUB_200_2011')
+            # if self.image_dir != 'images':
+            #     img_path = '/'.join([self.image_dir] + img_path.split('/')[idx+1:])
+            #     img_path = img_path.replace('images/', '')
+            # else:
+            #     img_path = '/'.join(img_path.split('/')[idx:])
             img = Image.open(img_path).convert('RGB')
         except:
             img_path_split = img_path.split('/')
--- class_attr_data_10/change_file_path.py
+++ class_attr_data_10/change_file_path.py
@@ -1,6 +1,6 @@
-import pickle
+import pickle, os
 
-split = 'val'
+split = 'test'
 data_path = f'class_attr_data_10/{split}.pkl'
 
 with open(data_path, 'rb') as f:
@@ -10,13 +10,19 @@ print(data[-1])
 
 # remove the path prefix
 prefix = '/juice/scr/scr102/scr/thaonguyen/CUB_supervision/datasets/'
-
+new_prefix = '/mnt/nasv3/hhz/Datasets/CUB200/'
 for d in data:
-    d['img_path'] = d['img_path'].replace(prefix, '')
+    d['img_path'] = d['img_path'].replace(prefix, new_prefix)
     
 print(data[-1]['img_path']) # CUB_200_2011/images/063.Ivory_Gull/Ivory_Gull_0005_49021.jpg
 
-save_path = f'CUB_processed/{split}.pkl'
+save_path = f'CUB_processed/class_attr_data_10/{split}.pkl'
 with open(save_path, 'wb') as f:
     pickle.dump(data, f)
 print(f'saved to {save_path}')
+
+# check if the image exist
+for d in data:
+    if not os.path.exists(d['img_path']):
+        print(d['img_path'])
+        break
